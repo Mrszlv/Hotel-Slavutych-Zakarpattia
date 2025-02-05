4acb32003a01b9757324ffff588f13b12f378418
Miroszlav <miroszlav.popovics@gmail.com>
2025-02-05 18:45:19 +0200

    commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,4 @@
-import { NavLink } from "react-router-dom";
-import clsx from "clsx";
-
 import s from "./Footer.module.css";
-
-const buildLinkClass = ({ isActive }) => {
-  return clsx(s.link, isActive && s.active);
-};
 
 const Footer = () => {
   return (
